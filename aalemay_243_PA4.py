8170e30b0a98e82f91eac7bd5004f17906c017eb
Afomya Sisay Alemayehu <98617020+afomya@users.noreply.github.com>
2022-03-13 21:16:24 -0400

    Update aalemay_243_PA4.py

--- aalemay_243_PA4.py
+++ aalemay_243_PA4.py
@@ -1,8 +1,3 @@
-# Part 1
-# are all the elements of animals in vertebrates?
-# if not, remove them.
-# find the max value
-# correspond the value with the animal
 # if animal is v
 
 animals = ["butterfly", "fly", "whale", "parrot"]
@@ -10,34 +5,39 @@ weights = [.0005, .000007, 300000, 3.52]
 vertebrates = ["hamster", "crocodile", "parrot", "whale"]
 pure_vertebrate = 0
 
-
-for animal in animals:
-    for vertebrate in vertebrates:
-        if animal == vertebrate:
-            pure_vertebrate.append(animal)
-            for pure in pure_vertebrate:
-                print(pure_vertebrate)
-                for i in range(10,000):
-                    if animal[i] == pure:
-                        final_verdict.append(weights[i])
-print(final_verdict)
 def biggest_vertebrate(animals, weights, vertebrates):
-    final_verdict = 0
+    final_verdict = " "
+    emp_variable = []
+    c = 0
+    max_animal = animals[c]
+    # It's really annoying how I can't find max weight cause of the restrictions
     max_weight = weights[0]
     for weight in weights:
         if weight > max_weight:
           max_weight = weight
-          for animal in animals:
-            index1 = weights.index(max_weight)
-            index2 = animals.index(animal)
-            if index1 == index2:
-                final_verdict = animal
-                for vertebrate in vertebrates:
-                    if animal == vertebrate:
-                        final_verdict = animal
-                    else:
-                        final_verdict = "None"
+
+    # correspond the value with the animal
+    for m in range(0, len(weights)):
+        if weights[m] == max_weight:
+            emp_variable.append(m)
+
+    c = emp_variable[0]
+    for vertebrate in vertebrates:
+        max_animal = animals[c]
+        if max_animal == vertebrate:
+            final_verdict = max_animal
+        else:
+            final_verdict = "None"
+
+
     return final_verdict
+
+
+print(biggest_vertebrate(["butterfly", "fly"], [0.0005, 0.000007], ["hamster","human", "mouse"]))
+
+
+
+
 
 
 
